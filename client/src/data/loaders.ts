35381c871276c5aefa8866b1834cc232e192778d
Norman <normanramadhan3@gmail.com>
2025-03-26 20:20:25 +0700

    create experience page

--- client/src/data/loaders.ts
+++ client/src/data/loaders.ts
@@ -34,12 +34,58 @@ const homePageQuery = qs.stringify({
   },
 });
 
+const BASE_URL = getStrapiURL();
 export async function getHomePage() {
   const path = "api/home-page";
-  const BASE_URL = getStrapiURL();
   const url = new URL(path, BASE_URL);
 
   url.search = homePageQuery;
 
   return fetchAPI(url.href, { method: "GET" });
+}
+
+const pageBySlugQuery = (slug: string) => qs.stringify(
+  {
+    filters: {
+      slug: {
+        $eq: slug,
+      },
+    },
+    populate: {
+      blocks: {
+        on: {
+          "blocks.hero-section": {
+            populate: {
+              image: {
+                fields: ["url", "alternativeText"],
+              },
+              logo: {
+                populate: {
+                  image: {
+                    fields: ["url", "alternativeText"],
+                  },
+                },
+              },
+              cta: true,
+            },
+          },
+          "blocks.info-blocks": {
+            populate: {
+              image: {
+                fields: ["url", "alternativeText"],
+              },
+              cta: true,
+            },
+          },
+        },
+      },
+    },
+  },
+);
+
+export async function getPageBySlug(slug: string) {
+  const path = "/api/pages";
+  const url = new URL(path, BASE_URL);
+  url.search = pageBySlugQuery(slug);
+  return await fetchAPI(url.href, { method: "GET" });
 }
